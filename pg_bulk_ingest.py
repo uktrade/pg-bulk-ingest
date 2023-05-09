389a94dbf6f94e7f1b6c6c6ec5dc0018a8e526b6
Michal Charemza <michal@charemza.name>
2023-05-09 12:06:03 +0100

    refactor: avoid duplication

--- pg_bulk_ingest.py
+++ pg_bulk_ingest.py
@@ -5,18 +5,21 @@ from psycopg import sql
 
 
 def upsert(conn, table, rows):
-    driver_connection = conn.connection.driver_connection
+
+    def bind_identifiers(query_str, *identifiers):
+        return sa.text(sql.SQL(query_str).format(
+            *(sql.Identifier(identifier) for identifier in identifiers)
+        ).as_string(conn.connection.driver_connection))
+
     table_name = uuid.uuid4().hex
 
     # Create an intermediate table like table
-    query = sa.text(sql.SQL('''
+    conn.execute(bind_identifiers('''
         CREATE SCHEMA IF NOT EXISTS {}
-    ''').format(sql.Identifier(table.schema)).as_string(driver_connection))
-    conn.execute(query)
-    query = sa.text(sql.SQL('''
+    ''', table.schema))
+    conn.execute(bind_identifiers('''
         CREATE TABLE {}.{}(id int)
-    ''').format(sql.Identifier(table.schema), sql.Identifier(table_name)).as_string(driver_connection))
-    conn.execute(query)
+    ''', table.schema, table_name))
 
     # Insert rows into that intermediate table
 
@@ -24,7 +27,6 @@ def upsert(conn, table, rows):
     # ON CONFLICT to update any existing rows
 
     # Drop the intermediate table
-    query = sa.text(sql.SQL('''
+    conn.execute(bind_identifiers('''
         DROP TABLE {}.{}
-    ''').format(sql.Identifier(table.schema), sql.Identifier(table_name)).as_string(driver_connection))
-    conn.execute(query)
+    ''', table.schema, table_name))
